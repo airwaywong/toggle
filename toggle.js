f3ab4f898572163c880f1040aa22891d0bcbbfe1
airwaywong <airwaywong@gmail.com>
2020-05-30 13:37:53 -0700

    initial commit

---
+++ toggle.js
@@ -0,0 +1,29 @@
+(function (global, factory) {
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = factory(require('jquery'));
+  } else if (typeof define === 'function' && define.amd) {
+    define('toggle', ['jQuery'], factory);
+  } else {
+    global.toggle = factory(global.jQuery);
+  }
+})(this, function ($) {
+
+  function init() {
+    $('[data-toggle]').each(function (idx, el) {
+      var $el = $(el);
+      var $target = $($el.data('toggle'));
+      $el.on('click', function (e) {
+        e.preventDefault();
+        if ($target.attr('hidden')) {
+          $target.removeAttr('hidden');
+        } else {
+          $target.attr('hidden', true);
+        }
+      });
+    });
+  }
+
+  return {
+    init: init
+  }
+})
